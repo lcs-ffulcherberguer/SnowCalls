801423939f3b10be55b91d53e27e400577f081d9
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-11-28 15:23:37 -0500

    Initial Commit

---
+++ SnowCalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCalls
+//
+//  Created by Fulcherberguer, Fernanda on 2019-11-28.
+//  Copyright © 2019 Fulcherberguer, Fernanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
