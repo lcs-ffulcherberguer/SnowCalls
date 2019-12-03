8f2c261faab5b54814a3b65964053dbd1fb16722
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-12-02 20:50:07 -0500

    Did the outlets and actions. Connected to the view controller.

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    //Made outlets to connet the View Crontroller
+    @IBOutlet weak var inputTextField: UITextView!
+    @IBOutlet weak var outputTextField: UITextView!
+    
+    //Made an variables for the numbers being created
+    var createNumberFromLoop = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
+    //Made an action for my convert button
+    @IBAction func numberConverter(_ sender: Any) {
+        //Made an guard for the input
+        guard let inputNumber = inputTextField.text, inputTextField.text != "" else {
+            outputTextField.text = "Please enter phones number to convert"
+            return
+        }
+        //Make variable and set to zero
+        
+        
+    }
+    
 
 }
 
